b92ab149fbae2435372eac62cd6bce50af463b0b
EmperorLeo <laf22@duke.edu>
2015-12-21 01:46:09 -0500

    added some more stuff to router

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ var userSchema = new Schema({
 	birthday: {type: String, required: true},
 	email: {type: String, required: true},
 	password: {type : String, required : true},
-	clubs : [{organization: String, color: String}],
+	clubs : [{organization: {type : String, required : true, unique : true}, color: String}],
 	meets: [{meetId: Number, date: Date, name: String, permission: Number}],
 	goals: [{meetId: Number, event: String, goalType: String, goal: String}],
 	sets: [{date: Date, name: String, workout: [{repetitions: Number, distance: Number, stroke: String, interval: String}]}],
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,79 @@ var router = express.Router();
 var bcrypt = require('bcrypt');
 var User = require('../models/user.js');
 
+
+/* This should post a user on user creation */
+router.post('/', function(req, res, next) {
+	
+	var user = new User();
+	
+	user.firstName = req.body.firstName;
+	user.lastName = req.body.lastName;
+	user.userName = req.body.userName;
+	user.birthday = req.body.birthday;
+	user.email = req.body.email;
+	
+	var salt = bcrypt.genSaltSync(10);
+	user.password = bcrypt.hashSync(req.body.password, salt);
+	
+	user.clubs = [];
+	user.meets = [];
+	user.goals = [];
+	user.sets = [];
+	user.times = [];
+	
+	user.save(function(err) {
+		
+		if(err) {
+			res.writeHead(400, {"Content-Type": "application/json"});
+			res.end(JSON.stringify({"created": false}));
+		} else {
+			res.writeHead(201, {"Content-Type": "application/json"});
+			var json = JSON.stringify({"created":true});
+			res.end(json); 
+		}
+
+	
+	});
+
+});
+
+
+router.post('/auth', function(req, res, next) {
+
+	var userName = req.body.userName;
+	var password = req.body.password;
+		
+	User.findOne({"userName": userName}, function(e, docs) {
+		
+		var user = docs;
+		
+		if(user) {
+		
+// 			console.log(user);
+		
+			var match = bcrypt.compareSync(password, user.password);
+		
+// 			console.log(match);
+		
+			if(match) {
+				res.writeHead(200, {"Content-Type": "application/json"});
+				res.end(JSON.stringify({"authorized":true, "token":"NOT_IMPLEMENTED"}));
+			} else {
+				res.writeHead(403, {"Content-Type": "application/json"});
+				res.end(JSON.stringify({"authorized":false}));
+			}
+		
+		} else {
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.end("User not found.");
+		}
+
+	
+	});	
+	
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 // 	var db = req.db;
@@ -37,76 +110,175 @@ router.get('/username/:username', function(req, res, next) {
 
 });
 
-
-/* This should post a user on user creation */
-router.post('/', function(req, res, next) {
+router.delete('/username/:username', function(req, res, next) {
 	
-	var user = new User();
-	
-	user.firstName = req.body.firstName;
-	user.lastName = req.body.lastName;
-	user.userName = req.body.userName;
-	user.birthday = req.body.birthday;
-	user.email = req.body.email;
-	
-	var salt = bcrypt.genSaltSync(10);
-	user.password = bcrypt.hashSync(req.body.password, salt);
-	
-	user.clubs = [];
-	user.meets = [];
-	user.goals = [];
-	user.sets = [];
-	user.times = [];
-	
-	user.save(function(err) {
+	User.findOne({userName: req.params.username}, function(err, user) {
 		
-		if(err) {
-			res.writeHead(400, {"Content-Type": "application/json"});
-			res.end(JSON.stringify({"created": false}));
-		} else {
-			res.writeHead(201, {"Content-Type": "application/json"});
-			var json = JSON.stringify({"created":true});
-			res.end(json);
-		}
+		if(user) {
+			
+			User.remove(user, function(error) {
+				res.statusCode = 204;
+				res.end();
+			});
 
+		} else {
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.end("User not found");
+		}
+	
+	});
+	
+});
+
+router.post('/:username/clubs', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		if(user) {
+						
+			
+			user.clubs.push({organization: req.body.organization, color: req.body.color});
+			user.save(function(err) {
+				
+				if(err) {
+					res.writeHead(400, {"Content-Type": "application/json"});
+					res.end(JSON.stringify({"success": false, "reason": "You dun screwed up.  This organization already exists."}));
+		
+				} else {
+					res.writeHead(200, {"Content-Type": "application/json"});
+					res.end(JSON.stringify({"success": true}));
+				}
+			});
+			
+					
+		} else {
+			res.writeHead(404, {"Content-Type": "application/json"});
+			res.end(JSON.stringify({"success": false, "reason": "User not found."}));
+		}
+	
+	});	
+
+	
+
+});
+
+/* Clears all clubs */
+router.delete('/:username/clubs', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		if(user) {
+			
+			user.clubs = [];
+			user.save(function(err) {
+				
+				res.statusCode = 204;
+				res.end();
+			
+			});
+			
+		} else {
+			res.writeHead(404, {"Content-Type": "application/json"});
+			res.end(JSON.stringify({"success": false, "reason": "User not found."}));
+		}
+	});	
+
+	
+});
+
+router.post('/:username/meets', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		if(user) {
+		
+		} else {
+		
+		}
 	
 	});
 
 });
 
-router.post('/auth', function(req, res, next) {
+router.delete('/:username/meets', function(req, res, next) {
 
-	var userName = req.body.userName;
-	var password = req.body.password;
+	User.findOne({userName: req.params.username}, function(err, user) {
 		
-	User.findOne({"userName": userName}, function(e, docs) {
-		
-		var user = docs;
-		
-		if(user) {
-		
-			console.log(user);
-		
-			var match = bcrypt.compareSync(password, user.password);
-		
-			console.log(match);
-		
-			if(match) {
-				res.writeHead(200, {"Content-Type": "application/json"});
-				res.end(JSON.stringify({"authorized":true, "token":"NOT_IMPLEMENTED"}));
-			} else {
-				res.writeHead(403, {"Content-Type": "application/json"});
-				res.end(JSON.stringify({"authorized":false}));
-			}
-		
-		} else {
-			res.writeHead(404, {"Content-Type": "text/plain"});
-			res.end("User not found.");
-		}
+		res.statusCode = 501;
+		res.end();
+	
+	});
 
-	
-	});	
-	
 });
+
+router.post('/:username/goals', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+router.delete('/:username/goals', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+router.post('/:username/times', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+router.delete('/:username/times', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+router.post('/:username/sets', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+router.delete('/:username/sets', function(req, res, next) {
+
+	User.findOne({userName: req.params.username}, function(err, user) {
+		
+		res.statusCode = 501;
+		res.end();
+	
+	});
+
+});
+
+
+
+
 
 module.exports = router;
